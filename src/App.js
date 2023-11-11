15bb49d0cfe0245990759bd30bcf4ce401bdc992
Isaac <isaacfrett2@gmail.com>
2023-11-11 12:57:03 -0600

    fixup: add footer and spaces to App

--- src/App.js
+++ src/App.js
@@ -6,17 +6,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={computer} className='background-image'></img>
-        <nav class="nav">
-          <a href="/" class="nav-logo-link"><img class="nav-logo" src={logo}/></a>
-          <ul class="nav-list">
-            <li class="nav-item"><a href="/" class="nav-link">Mission</a></li>
-            <li class="nav-item"><a href="/" class="nav-link">Resources</a></li>
-            <li class="nav-item"><a href="/" class="nav-link">About</a></li>
-          </ul>
-        </nav>
-      </header>
+      <img src={computer} className='background-image' alt='laptop in the background'></img>
+      <div class="nav">
+        <a href="/" class="nav-logo-link"><img class="nav-logo" src={logo} alt='logo of infantry endeavors'/></a>
+        <div class="nav-right">
+          <a href="/" class="nav-link">Mission</a>
+          <a href="/" class="nav-link">Resources</a>
+          <a href="/" class="nav-link">About</a>
+        </div>
+      </div>
+      <div className='middle-body'>
+
+      </div>
+      <div className='lower-body'>
+
+      </div>
+      <footer className='footer'>
+        <div className='footer-cols'>
+          <div className='col-left'>
+            <a href="/" class="footer-logo-link"><img class="footer-logo" src={logo} alt='logo of infantry endeavors'/></a>
+          </div>
+          <div className='divider'>
+
+          </div>
+          <div className='col-right'>
+            <a href="/" class="footer-link">Mission</a>
+            <a href="/" class="footer-link">Resources</a>
+            <a href="/" class="footer-link">About</a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
